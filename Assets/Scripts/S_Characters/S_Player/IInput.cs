361724d8dc70a0032457a911420f12643940b1a1
Pedro Gomez <pgomez519@gmail.com>
2024-08-14 12:22:38 -0400

    Add Demo Player Mov
    usando Input System

---
+++ Assets/Scripts/S_Characters/S_Player/IInput.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace S_Characters.S_Player
+{
+    public interface IInput
+    {
+        void OnEnable();
+        void OnDisable();
+        Vector2 GetPlayerMovement();
+    }
+}
