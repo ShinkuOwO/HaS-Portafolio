507c5bb0d6dca7d28f90c936df225f3a0488a096
Phobos29 <143896394+Phobos29@users.noreply.github.com>
2024-08-13 23:51:48 -0400

    add behavior player
    bla bla

---
+++ Assets/Scripts/BehaviorPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class BehaviorPlayer : MonoBehaviour
+{
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
